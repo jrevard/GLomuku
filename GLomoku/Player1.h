45c1cac188d75dbc78ca58315cfae43154b43676
Jared <jrodingtonesquire@gmail.com>
2012-03-22 15:08:27 -0700

    better

--- GLomoku/Player1.h
+++ GLomoku/Player1.h
@@ -23,16 +23,24 @@ class Player1 : public Player
 	
 		int test1;
 		int boardSize; // total moves on gameBoard
+		int pOneMoves;
 		int boardsExamined; // total boards looked at per call
 		int twoAr[2][100], threeAr[3][50], fourAr[4][10],
-			two, three, four;
+			two, three, four,
+			two_P_BB, two_P_BO, twp_P_OO,		// color 'P'
+			two_T_BB, two_T_BO, twp_T_OO,		// color 'T'
+			three_P_BB, three_P_BO, three_P_OO,	// Blocked Blocked
+			three_T_BB, three_T_BO, three_T_OO,	// Blocked Open
+			four_P_BB, four_P_BO, four_P_OO,	// Open Open
+			four_T_BB, four_T_BO, four_T_OO;
 	
 		char pOneBoard[BoardSize][BoardSize];
 	
 		bool isSolved(char &whoWon);
 		void TALLY_heur_vars(int &line, int &lineValue, char &playerColor);
-		int checkEnds(char &dir, int &line, int &row, int &col);
-		int totalEnds(int &line, char &leftVal, char &rightVal);
+		
+		int checkEnds(char &dir, char &moveColor, int &line, int &row, int &col);
+		int totalEnds(int &line, char &moveColor, char &leftVal, char &rightVal);
 
 		bool playerDetected(int row, int col);
 	
